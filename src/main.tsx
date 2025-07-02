0366e25fe3d7b830b07343cbe645086e5e73e7cf
macinjoke <shunji.makino@gmail.com>
2025-07-02 22:08:24 +0900

    修正

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const reactRootDiv = document.getElementById('root')
+if (!reactRootDiv) {
+  throw new Error(
+    'root element not found. Please ensure there is a <div id="root"></div> in your index.html.',
+  )
+}
+
+ReactDOM.createRoot(reactRootDiv).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
